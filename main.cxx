2daac75558376a282b84824c5d4cf8aee5129229
Sergey Dyachkov <serj.dyachkov@gmail.com>
2018-01-25 15:13:59 +0300

    lesson 1 finished

---
+++ main.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <main.h>
+
+int main() {
+    std::cout << "Smoothed CMake Learning, " 
+              << "version "
+              << VERSION_MAJOR << "."
+              << VERSION_MINOR << "."
+              << VERSION_PATCH << std::endl;
+}
